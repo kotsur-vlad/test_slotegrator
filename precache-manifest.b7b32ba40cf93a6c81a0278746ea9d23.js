6ab54c1766f0c8338bb485c21e8c5c5f179555a8
kotsur-vlad <kotsur.vlad@gmail.com>
2020-09-21 14:18:07 +0300

    Updates

---
+++ precache-manifest.b7b32ba40cf93a6c81a0278746ea9d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6205661184e5b427bf725f194bd1acc",
+    "url": "/test_slotegrator/index.html"
+  },
+  {
+    "revision": "3850a80de4cf8dfd63f0",
+    "url": "/test_slotegrator/static/css/main.0b2ac838.chunk.css"
+  },
+  {
+    "revision": "95c8517dd76dfaffdf57",
+    "url": "/test_slotegrator/static/js/2.5720add7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test_slotegrator/static/js/2.5720add7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3850a80de4cf8dfd63f0",
+    "url": "/test_slotegrator/static/js/main.ab16f5bf.chunk.js"
+  },
+  {
+    "revision": "95f5591f259c993f4a03",
+    "url": "/test_slotegrator/static/js/runtime-main.dbed94e9.js"
+  }
+]);
